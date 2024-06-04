a6698a0073d4b3196802bc7c5632038eea5db595
xtycoon <125221464+xtycoonfi@users.noreply.github.com>
2024-06-04 08:41:39 +0200

    Update buy_token_random.ts

--- _execute/buy_token_random.ts
+++ _execute/buy_token_random.ts
@@ -86,7 +86,7 @@ export async function buyTokenRandom(skcrypted: string, token: string) {
             // const signature = await sendTransactionWrapper(transaction, [payer])
 
             // REMOVE THE AWAIT HERE IF YOU DONT WANT TO WAIT THE CONFIRMATION
-            // const signature = sendAndConfirmTransactionWrapper(connection, transaction, [payer])
+            // const signature = await sendAndConfirmTransactionWrapper(connection, transaction, [payer])
             const signature = sendAndConfirmTransactionWrapper(connection, transaction, [payer])
             console.log('> Buy transaction confirmed:', signature)
         }
